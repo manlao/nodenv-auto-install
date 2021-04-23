b9450ab8c3ce225cff8e02a08dc060db046bdf36
Sean Tu <manlao@gmail.com>
2021-04-23 15:34:44 +0800

    feat: fix stdout issue

--- etc/nodenv.d/which/auto-install.bash
+++ etc/nodenv.d/which/auto-install.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ "$NODENV_VERSION" != "system" ] && [ ! -d "$NODENV_ROOT/versions/$NODENV_VERSION" ]; then
-  nodenv install "$NODENV_VERSION"
+  nodenv install "$NODENV_VERSION" 1>&2
 fi
