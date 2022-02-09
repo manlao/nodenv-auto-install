6b1686ff39b826c12af94acd537a296f94b3689f
Sean Tu <manlao@gmail.com>
2022-02-09 19:26:05 +0800

    feat: Skip if the version appears to be installed already

--- etc/nodenv.d/version-name/z-auto-install.bash
+++ etc/nodenv.d/version-name/z-auto-install.bash
@@ -3,5 +3,5 @@ if [ "$NODENV_VERSION" != "system" ] \
   && [ ! -d "${NODENV_ROOT}/versions/${NODENV_VERSION/v/}" ] \
   && [ ! -d "${NODENV_ROOT}/versions/${NODENV_VERSION#node-}" ] \
   && [ ! -d "${NODENV_ROOT}/versions/${NODENV_VERSION#node-v}" ] ; then
-  nodenv install "$NODENV_VERSION" 1>&2
+  nodenv install -s "$NODENV_VERSION" 1>&2
 fi
